14fcf41eb9b306f1f7f8314f0e44452e00df1af4
Eugene Skepner <github@skepner.eu>
2017-10-18 17:24:48 +0200

    path drawing bug fix

--- cc/unused/geographic-map.cc
+++ cc/unused/geographic-map.cc
@@ -11,7 +11,7 @@ void ColoringByGeographyMapLegend::draw(Surface& aSurface) const
     double geographic_map_outline_width = 1;
     // cairo_path_t* map_outline = outline(aSurface, geographic_map_path);
     // const double scale = aSurface.draw_path_scale(map_outline, aViewport);
-    aSurface.path_outline(geographic_map::path.begin(), geographic_map::path.end(), geographic_map_outline_color, geographic_map_outline_width);
+    aSurface.path_outline_negative_move(geographic_map::path.begin(), geographic_map::path.end(), geographic_map_outline_color, geographic_map_outline_width);
     // for (const auto& label: ColoringByContinentLegendLabels) {
     //     cairo_path_t* continent_outline = outline(aSurface, continent_path(label));
     //     aSurface.draw_path(continent_outline, aViewport, scale, mColoring.color(label), aSettings.geographic_map_outline_width);
